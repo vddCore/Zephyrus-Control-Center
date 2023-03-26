25708e93a59dd84976d3f2d1af62f1710353ac3e
vddCore <vddcore@outlook.com>
2023-03-26 23:58:14 +0200

    keyboard stuff is Done.

--- Slate/Controller/ApplicationController.Keyboard.cs
+++ Slate/Controller/ApplicationController.Keyboard.cs
@@ -1,8 +1,13 @@
-﻿using Glitonea.Mvvm.Messaging;
+﻿using System.Diagnostics;
+using Avalonia;
+using Avalonia.Threading;
+using Glitonea.Extensions;
+using Glitonea.Mvvm.Messaging;
 using Slate.Infrastructure.Asus;
 using Slate.Model;
 using Slate.Model.Messaging;
 using Slate.Model.Settings.Components;
+using Slate.View.Window;
 
 namespace Slate.Controller
 {
@@ -20,36 +25,50 @@ namespace Slate.Controller
             switch (ev)
             {
                 case AtkWmiEventID.KeyPressM3:
-                    HandleKeyPress(KeyboardSettings.BindM3, ev);
+                    HandleKeyPress(KeyboardSettings.BindM3);
                     break;
-                
+
                 case AtkWmiEventID.KeyPressM4:
-                    HandleKeyPress(KeyboardSettings.BindM4, ev);
+                    HandleKeyPress(KeyboardSettings.BindM4);
                     break;
-                
+
                 case AtkWmiEventID.KeyPressFnF4:
-                    HandleKeyPress(KeyboardSettings.BindF4, ev);
+                    HandleKeyPress(KeyboardSettings.BindF4);
                     break;
-                
+
                 case AtkWmiEventID.KeyPressFnF5:
-                    HandleKeyPress(KeyboardSettings.BindF5, ev);
+                    HandleKeyPress(KeyboardSettings.BindF5);
                     break;
             }
         }
 
-        private void HandleKeyPress(KeyBind keyBind, AtkWmiEventID ev)
+        private void HandleKeyPress(KeyBind keyBind)
         {
             switch (keyBind.Mode)
             {
                 case KeyBindMode.Default:
+                {
+                    Dispatcher.UIThread.Post(() =>
+                    {
+                        Message.Broadcast<TrayIconClickedMessage>();
+                    });
+                    
                     break;
-                
+                }
+
                 case KeyBindMode.Media:
                     _inputInjectionService.SimulateMediaKeyPress(keyBind.MediaKey);
                     break;
-                
+
                 case KeyBindMode.Command:
+                {
+                    new Process
+                    {
+                        StartInfo = new ProcessStartInfo(keyBind.Command ?? string.Empty)
+                    }.Start();
+                    
                     break;
+                }
             }
         }
 
